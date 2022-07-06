c619bb76d5b89ee9d12ca98539cf63e2a30d8785
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-07-06 15:59:35 +0100

    ready for review

--- server/models/games-models.js
+++ server/models/games-models.js
@@ -94,3 +94,22 @@ exports.fetchUsers = () => {
     return rows;
   });
 };
+
+exports.insertCommentByReviewID = (review_id, body, username) => {
+  if (typeof body !== "string") {
+    return Promise.reject({ status: 400, message: "Invalid request." });
+  }
+  return connection
+    .query(
+      `
+  INSERT INTO comments
+  (review_id, body, author)
+  VALUES
+  ($1, $2, $3)
+  RETURNING *`,
+      [review_id, body, username]
+    )
+    .then(({ rows: [comment] }) => {
+      return comment;
+    });
+};
