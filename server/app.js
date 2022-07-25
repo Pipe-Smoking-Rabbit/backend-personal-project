99f8321b78fcdde057dca4776799888496d33044
Pipe-Smoking-Rabbit <mitchplease02@gmail.com>
2022-07-25 10:47:38 +0100

    allow cross origin resource sharing

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const cors = require("cors")
 const express = require("express");
 const {
   getCategories,
@@ -13,6 +14,7 @@ const {
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", getAPI)
